30a381586629034207e73ea4ab2ef2dc1b570543
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-01 23:18:15 +0600

    Ternary operator, Optional Chaining, Nullish Coalescing Operator

---
+++ module1/src/1.11.ts
@@ -0,0 +1,11 @@
+{
+    //Ternary operator | Optional Chaining | Nullish Coalescing Operator:
+
+    const age: number = 10;
+
+    if(age > 15){
+        console.log("Adult");
+    }else{
+        console.log("Not Adult");
+    }
+}
