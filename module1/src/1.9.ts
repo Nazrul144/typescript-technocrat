271b3cb08c0ed784a3e0492ec782d26c7cf93693
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-01 22:06:21 +0600

    alias practice

---
+++ module1/src/1.9.ts
@@ -0,0 +1,24 @@
+{
+    //Type alias:
+    type Student = {
+        name: string;
+        age: number;
+        address: string;
+        phone: string;
+        email?: string;
+    }
+    const student1: Student = {
+        name: 'Nazrul',
+        age: 26,
+        address: 'Dhaka',
+        phone: '01758752528'
+    }
+    const student2: Student = {
+        name: 'Miraz',
+        age: 30,
+        address: 'Khulna',
+        phone: '01710326590'
+    }
+
+
+}
