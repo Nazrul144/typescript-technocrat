e2a50c97f0ea7b38de457794a283b9bf8e26d403
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-01 17:22:09 +0600

    Spread Operator practice with object

--- module1/src/1.7.ts
+++ module1/src/1.7.ts
@@ -9,4 +9,23 @@
     const bros2: string[] = ['Tarek', 'Sojib', 'Rakib'];
 
     bros1.push(...bros2)
+
+    const mentor1 = {
+        typescript: 'Mezba',
+        redux: 'Mir',
+        dbms: 'Mizan'
+    }
+
+    const mentor2 = {
+        prizma: "Firoz",
+        next: 'Tanmoy',
+        cloud: 'Nahid'
+    }
+
+    const mentorList = {
+        ...mentor1,...mentor2
+    }
+    console.log(mentorList);
+
+
 }
