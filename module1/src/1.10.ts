d2b155263c5a8aaf6be80dee4dd93d098d89190a
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-01 22:54:23 +0600

    Union Types Practices

--- module1/src/1.10.ts
+++ module1/src/1.10.ts
@@ -4,4 +4,19 @@
     type FrontendDeveloper = 'fakibazDeveloper' | 'juniorDeveloper'
 
     const newDeveloper: FrontendDeveloper = 'fakibazDeveloper'
+
+    type User = {
+        name: string;
+        email?: string
+        age: number;
+        gender: 'male' | 'female'
+    }
+
+    const user1 : User = {
+        name: 'Nazrul',
+        age: 30,
+        gender: 'male'
+    }
+
+
 }
