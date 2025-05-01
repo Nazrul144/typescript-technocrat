218a7e5d9badd34bdf7d58d154889de5a9a3ec2d
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-01 17:10:48 +0600

    Spread Operator practice

---
+++ module1/src/1.7.ts
@@ -0,0 +1,14 @@
+//Spread Operator
+//Rest Operator
+//Destructuring
+
+{
+
+    //Spread Operator:
+    const num1 = [1,2,3,4];
+    const num2 = [5,6,7,8];
+
+    const mainArr = [...num1,...num2]
+    console.log(mainArr);
+
+}
