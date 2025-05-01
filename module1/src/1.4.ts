00314d82eba73e1d944031bd70023bfcc3d756c2
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-01 12:13:22 +0600

    Typescript Primitive Datatype assigned

---
+++ module1/src/1.4.ts
@@ -0,0 +1,20 @@
+{//Basic Data type:
+
+//String Data type:
+let firstName: string = 'Nazrul';
+
+//Number Data Type:
+let rollNumber: number = 211002144;
+
+
+//Boolean Data Type:
+let isAdmin:boolean = true;
+
+//Null Data Type:
+let a: null = null;
+
+//Undefined Data Type:
+let b: undefined = undefined;
+
+
+}
