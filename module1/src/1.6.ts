563f7f93f3c6fec599a7bfe8ade9d939d580f549
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-01 14:04:11 +0600

    Method logic is implemented in object

--- module1/src/1.6.ts
+++ module1/src/1.6.ts
@@ -3,7 +3,7 @@
 //Normal Function
 //Arrow Function
 
-function  add(num1: number, num2: number): number{
+function  add(num1: number, num2: number = 100): number{
     return num1 + num2;
 }
 add(10,20)
@@ -11,3 +11,18 @@ add(10,20)
 const sum = (num1: number, num2: number): number =>  num1 + num2;
 
 sum(20,30);
+
+//Object --> function = method. If any function is written in the object this time it's called method.
+
+const poorUser = {
+    name: 'Nazrul',
+    balance: 0,
+    addBalance(balance: number): string{
+        return `My balance is: ${this.balance + balance}`
+    }
+
+}
+
+const arr = [1,2,3,4,5];
+
+const newArr: number[] = arr.map((element: number): number => element*element)
