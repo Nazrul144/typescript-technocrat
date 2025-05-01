6f9459c5f37f41938caeae62cd0305c80a846ae3
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-01 23:32:21 +0600

    Nullish coalescing Operator practice

--- module1/src/1.11.ts
+++ module1/src/1.11.ts
@@ -1,11 +1,22 @@
 {
     //Ternary operator | Optional Chaining | Nullish Coalescing Operator:
 
-    const age: number = 10;
+    const age: number = 18;
 
     if(age > 15){
         console.log("Adult");
     }else{
         console.log("Not Adult");
     }
+
+    const isAdult = age > 18 ? 'Adult' : "Not Adult"
+    console.log({isAdult});
+
+    //Nullish coalescing Operator:
+    //When nullish coalescing operator is used? null--> undefined: depending on null and undefined if I take any decision this time I can use it.
+
+    const isAuthenticated = "";
+    const result1 = isAuthenticated ?? 'Geust'
+    const result2 = isAuthenticated ? isAuthenticated : "Geust";
+    console.log({result1}, {result2});
 }
