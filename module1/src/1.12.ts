4c2be638769ed867fce81663119331246f776ab3
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-01 23:45:54 +0600

    nullable type practice with logic

---
+++ module1/src/1.12.ts
@@ -0,0 +1,11 @@
+{
+    //nullable type:
+    const searchName = (value: string)=>{
+        if(value){
+            console.log("Searching");
+        }else{
+            console.log("There is nothing to search");
+        }
+    }
+    searchName('Nazrul')
+}
