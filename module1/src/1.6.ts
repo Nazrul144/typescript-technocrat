d75b3f188a9a3d7474a59fdc9fa9914fc0d395cb
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-02 16:40:02 +0600

    Spread Operator practice with object

--- module1/src/1.6.ts
+++ module1/src/1.6.ts
@@ -1,35 +1,28 @@
-// //Function in Typescript:
+//Function in Typescript:
 
-// //Normal Function
-// //Arrow Function
+//Normal Function
+//Arrow Function
 
-// function  add(num1: number, num2: number = 100): number{
-//     return num1 + num2;
-// }
-// add(10,20)
+function  add(num1: number, num2: number = 100): number{
+    return num1 + num2;
+}
+add(10,20)
 
-// const sum = (num1: number, num2: number): number =>  num1 + num2;
+const sum = (num1: number, num2: number): number =>  num1 + num2;
 
-// sum(20,30);
+sum(20,30);
 
-// //Object --> function = method. If any function is written in the object this time it's called method.
+//Object --> function = method. If any function is written in the object this time it's called method.
 
-// const poorUser = {
-//     name: 'Nazrul',
-//     balance: 0,
-//     addBalance(balance: number): string{
-//         return `My balance is: ${this.balance + balance}`
-//     }
+const poorUser = {
+    name: 'Nazrul',
+    balance: 0,
+    addBalance(balance: number): string{
+        return `My balance is: ${this.balance + balance}`
+    }
 
-// }
+}
 
-// const arr = [1,2,3,4,5];
+const arr = [1,2,3,4,5];
 
-// const newArr: number[] = arr.map((element: number): number => element*element)
-
-
-const numbers:number[] = [1,2,3,4,5];
-
-const totalResult = numbers.reduce((acc: number, current: number): number=> acc + current, 0)
-
-console.log(totalResult);
+const newArr: number[] = arr.map((element: number): number => element*element)
--- module1/src/1.7.ts
+++ module1/src/1.7.ts
@@ -1,43 +1,52 @@
-//Spread Operator
-//Rest Operator
-//Destructuring
+// //Spread Operator
+// //Rest Operator
+// //Destructuring
 
-{
+// {
 
-    //Spread Operator:
-    const bros1: string[] = ['Mizan', 'Raju', 'Rahad'];
-    const bros2: string[] = ['Tarek', 'Sojib', 'Rakib'];
+//     //Spread Operator:
+//     const bros1: string[] = ['Mizan', 'Raju', 'Rahad'];
+//     const bros2: string[] = ['Tarek', 'Sojib', 'Rakib'];
 
-    bros1.push(...bros2)
+//     bros1.push(...bros2)
 
-    const mentor1 = {
-        typescript: 'Mezba',
-        redux: 'Mir',
-        dbms: 'Mizan'
-    }
+//     const mentor1 = {
+//         typescript: 'Mezba',
+//         redux: 'Mir',
+//         dbms: 'Mizan'
+//     }
 
-    const mentor2 = {
-        prizma: "Firoz",
-        next: 'Tanmoy',
-        cloud: 'Nahid'
-    }
+//     const mentor2 = {
+//         prizma: "Firoz",
+//         next: 'Tanmoy',
+//         cloud: 'Nahid'
+//     }
 
-    const mentorList = {
-        ...mentor1,...mentor2
-    }
-    console.log(mentorList);
+//     const mentorList = {
+//         ...mentor1,...mentor2
+//     }
+//     console.log(mentorList);
 
- //Rest Operator:
- const friends = (friend1: string, friend2: string,friend3: string)=>{
-    console.log(`Hi ${friend1} ${friend2} ${friend3}`);
- }
- friends("Abut", "Kabul","Tabul", )
+//  //Rest Operator:
+//  const friends = (friend1: string, friend2: string,friend3: string)=>{
+//     console.log(`Hi ${friend1} ${friend2} ${friend3}`);
+//  }
+//  friends("Abut", "Kabul","Tabul", )
 
- const greetingFriends = (...friends: string[])=>{
-    friends.forEach((friend: string)=> console.log(`Hi ${friend}`))
- }
- greetingFriends("Abut", "Kabul","Tabul","Ubul","Rasel", "Shahidul", "Majed", "Rabbi" )
+//  const greetingFriends = (...friends: string[])=>{
+//     friends.forEach((friend: string)=> console.log(`Hi ${friend}`))
+//  }
+//  greetingFriends("Abut", "Kabul","Tabul","Ubul","Rasel", "Shahidul", "Majed", "Rabbi" )
 
 
 
-}
+// }
+
+
+//Spread Operator:
+
+const friend1 = ['Rahim', 'Karim'];
+const friend2 = ['Jadu', 'Modu'];
+
+const newArray = [...friend1,...friend2]
+console.log(newArray);
