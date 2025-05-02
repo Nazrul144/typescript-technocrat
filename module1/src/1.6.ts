b2115bf9530538f1b5c4ba9c8160fdc5f8f0c5f8
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-02 16:15:57 +0600

    Function practice with typescript

--- module1/src/1.6.ts
+++ module1/src/1.6.ts
@@ -1,28 +1,35 @@
-//Function in Typescript:
+// //Function in Typescript:
 
-//Normal Function
-//Arrow Function
+// //Normal Function
+// //Arrow Function
 
-function  add(num1: number, num2: number = 100): number{
+// function  add(num1: number, num2: number = 100): number{
+//     return num1 + num2;
+// }
+// add(10,20)
+
+// const sum = (num1: number, num2: number): number =>  num1 + num2;
+
+// sum(20,30);
+
+// //Object --> function = method. If any function is written in the object this time it's called method.
+
+// const poorUser = {
+//     name: 'Nazrul',
+//     balance: 0,
+//     addBalance(balance: number): string{
+//         return `My balance is: ${this.balance + balance}`
+//     }
+
+// }
+
+// const arr = [1,2,3,4,5];
+
+// const newArr: number[] = arr.map((element: number): number => element*element)
+
+function add(num1: number, num2: number): number{
     return num1 + num2;
 }
-add(10,20)
 
-const sum = (num1: number, num2: number): number =>  num1 + num2;
-
-sum(20,30);
-
-//Object --> function = method. If any function is written in the object this time it's called method.
-
-const poorUser = {
-    name: 'Nazrul',
-    balance: 0,
-    addBalance(balance: number): string{
-        return `My balance is: ${this.balance + balance}`
-    }
-
-}
-
-const arr = [1,2,3,4,5];
-
-const newArr: number[] = arr.map((element: number): number => element*element)
+add(10,20);
+console.log("The result", add);
