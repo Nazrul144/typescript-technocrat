91af0d348aa827a39df8d5108129d85f62b2ebec
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-03 18:22:39 +0600

    Assertion practice with typesectipt

--- module2/2.1.ts
+++ module2/2.1.ts
@@ -7,12 +7,24 @@
 
     (anything as number)
 
-    const kgToGm = (value: string | number)=>{
+    const kgToGm = (value: string | number): string | number | undefined =>{
         if(typeof value === 'string'){
             const convertedNumber = parseFloat(value)*1000;
             return convertedNumber;
         }else{
             return value*1000;
         }
+    }
+    const result1 = kgToGm(1000) as number
+    const result2 = kgToGm("1000") as string
+
+    type CustomError = {
+        message: string;
+    }
+
+    try {
+        
+    } catch (error) {
+        console.log((error as CustomError).message);
     }
 }
