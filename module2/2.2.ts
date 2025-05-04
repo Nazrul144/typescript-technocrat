af03bc0236ba1f23d4576848cb216c5ebe809e29
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-04 23:11:47 +0600

    Interface practice with type declaration

--- module2/2.2.ts
+++ module2/2.2.ts
@@ -1,16 +1,30 @@
 {
     //Interface
-    type User1 = {
-        name: string;
-        age: number
-    }
+  
+   type Person1 = {
+    name: string;
+    age: number
+   }
 
-    type UserWithRoll = User1 & {roll: number};
+   type Person2 = {
+    address: 'Dhaka'
+   }
 
-    const student : UserWithRoll = {
-        name: "Nazrul Islam",
-        age: 20,
-        roll: 1,
+   interface PersonWithAllInfo extends Person1 {
+    isMarried?: boolean;
+    blood: string;
+    skin: string;
+    money: number
+   }
 
-    }
+   const final : PersonWithAllInfo = {
+    name: "Nazrul Islam",
+    age: 20,
+    isMarried: false,
+    blood:"A+",
+    skin: "white",
+    money: 2000,
+   }
+   console.log({final});
+
 }
