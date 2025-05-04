e4cf91b16666c245c0c3a408eed5409039db754b
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-04 22:41:42 +0600

    Interface practice

---
+++ module2/2.2.ts
@@ -0,0 +1,22 @@
+{
+    //Interface
+   type User1 = {
+    name: string,
+    age: number
+   }
+
+   const person : User1 = {
+    name: 'Nazrul',
+    age: 20
+   }
+
+   interface User2 {
+    id: number,
+    email: string
+   }
+
+   const person1 : User2 ={
+    id: 1,
+    email: 'nazrulislam.cse28@gmail.com'
+   }
+}
