5a018946275086086068cec50868074fb4d5e4ca
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-04 23:27:21 +0600

    Interface practice with type declaration

--- module2/2.2.ts
+++ module2/2.2.ts
@@ -6,10 +6,6 @@
     age: number
    }
 
-   type Person2 = {
-    address: 'Dhaka'
-   }
-
    interface PersonWithAllInfo extends Person1 {
     isMarried?: boolean;
     blood: string;
@@ -26,5 +22,23 @@
     money: 2000,
    }
    console.log({final});
+
+   type User1 = {
+    name: string,
+    age: number
+   }
+
+   type Final = User1 & {email: string}
+
+
+   type Roll1 = number[]; //Array declaration with type signature:
+   interface Roll2 {
+    [index: number] : number  //Array declaration with interface signature:
+   }
+
+   const rollNumber: Roll1 = [1,2,3,4];
+    
+   
+
 
 }
