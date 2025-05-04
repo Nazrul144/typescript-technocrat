6b482ccdf0d7217ccc8c3271f3e54a3f50bcd8e4
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-05 00:16:12 +0600

    Array generalized practice

--- module2/2.3.ts
+++ module2/2.3.ts
@@ -1,13 +1,18 @@
 {
-    //Generic Type
+    //Generic 
+    
+    type GenericType<param> = Array<param>
+
+
+
     const rollNumber : number[] = [1,2,3,4];        //Not generalized 
-    const rollNumber1 : Array<number> = [1,2,3,4]; // Generalized array
+    const rollNumber1 : GenericType<number> = [1,2,3,4]; // Generalized array
 
     const mentor : string[] = ['Mr.X', 'Mr.Y', 'Mr.Z'];       //Not generalized
-    const mentor1 : Array<string> = ['Mr.X', 'Mr.Y', 'Mr.Z']; //Generalized array
+    const mentor1 : GenericType<string> = ['Mr.X', 'Mr.Y', 'Mr.Z']; //Generalized array
 
     const boolArr : Array<boolean> = [true, false, true];   //Not generalized
-    const boolArr1 : Array<boolean> = [true, false, true];  //Generalized array
+    const boolArr1 : GenericType<boolean> = [true, false, true];  //Generalized array
 
 
 
