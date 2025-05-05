e3e20a99a65150f824009eab49c0b99bcfe48806
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-06 00:03:31 +0600

    Conditional Type Practice

--- module2/2.9.ts
+++ module2/2.9.ts
@@ -2,9 +2,14 @@
     //Conditional type:
 
     type a1 = number;
-    type b1 = undefined;
+    type b1 = string;
 
     type x = a1 extends null ? true : false
 
     type y = a1 extends null ? true : b1 extends undefined ? undefined : any
+
+    type CheckVehicle<T> = T extends "car" | "bike" | "ship" ? true : false
+
+    type HasCar = CheckVehicle<"car">
+
 }
