ce8303d0dedc83942fe29856dfae96b0edbba8e7
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-06 01:04:15 +0600

    Required utility practice

--- module2/2.11.ts
+++ module2/2.11.ts
@@ -6,8 +6,12 @@
         email?: string;
         contactNo: string;
     }
-
+    //Pick:
     type NameAge = Pick<Person, "name" | "age"> //We can pick some object property with value by Pick keyword and make a new object.
 
+    //Omit:
     type contactInfo = Omit<Person, "name" | "contactNo">
+
+    //Required:
+    type NewRequired = Required<Person>
 }
