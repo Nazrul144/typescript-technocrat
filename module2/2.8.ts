acb637cc982592aaa02ffb409f47ae1fd9e92af1
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-05 23:33:53 +0600

    Practice asynchronous async await

---
+++ module2/2.8.ts
@@ -0,0 +1,20 @@
+{
+    //promise
+
+    const createPromise = ()=>{
+        return new Promise((resolve, reject)=>{
+            const data : string = "something"
+            if(data){
+                resolve(data)
+            }else{
+                reject("Fail to load data")
+            }
+        })
+    }
+
+    const showData = async()=>{
+        const data = await createPromise()
+        console.log(data);
+    }
+    showData()
+}
