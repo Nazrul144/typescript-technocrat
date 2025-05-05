8a90e7ccb01e17c2fac7533e4a041a0eecbb2856
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-05 22:45:57 +0600

    Practice constrain

---
+++ module2/2.6.ts
@@ -0,0 +1,31 @@
+{
+  //Constrain
+
+  const addCourseToStudent = <T extends {id: number, name: string, email: string}>(student: T) => {
+    const course = "Next Level Web Development";
+
+    return {
+      ...student,
+      course,
+    };
+  };
+
+  const student1 = addCourseToStudent<{
+    id: number;
+    name: string;
+    email: string;
+    devType: string;
+  }>({ id: 1, name: "Mr.X", email: "x@gmail.com", devType: "Full Stack" });
+
+  const student2 = addCourseToStudent<{
+    id: number;
+    name: string;
+    email: string;
+    hasWatch: string;
+  }>({ id: 2, name: "Mr.Y", email: "y@gmail.com", hasWatch: "Olive" });
+
+  console.log(student1);
+  console.log(student2);
+
+  //
+}
