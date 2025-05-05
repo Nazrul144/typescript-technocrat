af101149b61635876b23078faee5dd8fdd80bfe2
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-06 00:52:08 +0600

    mapped type array operation

--- module2/2.10.ts
+++ module2/2.10.ts
@@ -3,6 +3,22 @@
     const arrOfNumbers : number[] = [1,4, 5];
     const arrOfSting : string[] = ["1", "4", "5"];
 
-    const newStringArr = arrOfNumbers.toString();
+    const newStringArr : string[] = arrOfNumbers.map((number)=> number.toString())
     console.log(newStringArr);
+
+    type AreaNumber = {
+        height: number;
+        width: number;
+    }
+
+    type Height = AreaNumber["height"]  //look up type
+
+    type AreaString<T> = {
+        [key in keyof T] : T[key]
+    }
+
+    const area1 : AreaString<{height:string; width: number}> = {
+        height: "100",
+        width: 40
+    }
 }
