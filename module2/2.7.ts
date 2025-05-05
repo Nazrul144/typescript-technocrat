0ad10d4b082ba8091525de1038bb10f40133302d
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-05 23:21:19 +0600

    Create function with object and key to get the object value

--- module2/2.7.ts
+++ module2/2.7.ts
@@ -14,15 +14,21 @@
         name:string, roll: number, age: number
     }
 
-    const getPropertyValue = (obj: object, key: string) =>{
-        return obj
+    const getPropertyValue = <X, Y extends keyof X>(obj: X, key: Y) =>{
+        return obj[key]
     }
 
-    // const user: Student = {
-    //     name: "Nazrul Islam",
-    //     roll: 12,
-    //     age: 30,
-    // }
+    const user: Student = {
+        name: "Nazrul Islam",
+        roll: 12,
+        age: 30,
+    }
+    const car = {
+        model: "ABC1",
+        year: 2000,
+    }
+
+    const result1 = getPropertyValue(car, "model")
 
     
 }
