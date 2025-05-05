642ead74775584c3953e988582542fa8d1087855
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-05 23:10:07 +0600

    Create function with object and key to get the object value

--- module2/2.7.ts
+++ module2/2.7.ts
@@ -1,10 +1,28 @@
 {
     //Generic constrain with keyof operator:
-    type Vehecle = {
+    type Vehicle = {
         bike: string;
         car: string;
         ship: string;
     }
 
     type Owner = "Car" | "Bike" | "Ship"  //Manually type declaration
+
+    type Owner2 = keyof Vehicle;
+
+    type Student = {
+        name:string, roll: number, age: number
+    }
+
+    const getPropertyValue = (obj: object, key: string) =>{
+        return obj
+    }
+
+    // const user: Student = {
+    //     name: "Nazrul Islam",
+    //     roll: 12,
+    //     age: 30,
+    // }
+
+    
 }
