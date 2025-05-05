575c6debed085078895865afdcb91b418a259c72
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-06 00:33:38 +0600

    mapped type array operation

---
+++ module2/2.10.ts
@@ -0,0 +1,8 @@
+{
+    //mapped type:
+    const arrOfNumbers : number[] = [1,4, 5];
+    const arrOfSting : string[] = ["1", "4", "5"];
+
+    const newStringArr = arrOfNumbers.toString();
+    console.log(newStringArr);
+}
