6058cfa86ef592f537a307515f10c798618d752d
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-06 12:23:41 +0600

    Practice task 1 done

---
+++ practiceTask/task1.ts
@@ -0,0 +1,5 @@
+{
+    //Write a typeScript program that outputs a welcome message:
+    const name: string = 'Hello World, I will complete this course successfully and become a Next Level Web Developer'
+    console.log(name);
+}
