279348de0f231b34e981ad71ad0fb39e16abbb48
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-05-07 17:39:14 +0600

    Create teacher classes with name, age, address and designation value

--- module3/3.2.ts
+++ module3/3.2.ts
@@ -11,12 +11,54 @@
             this.address = address;
         }
 
-        callStudnent(){
-            console.log(`Hi, I am ${this.name} and I am ${this.age} old. My address is: ${this.address}.`);
+        getSleep(sleepOfHour : number){
+            console.log(`${this.name} will sleep ${sleepOfHour}`);
         }
+       
     }
 
     const studnet1 = new Student("Nazrul Islam", 30, "Dhaka")
-    studnet1.callStudnent()
+    studnet1.getSleep(10)
+
+    class Teacher {
+        name: string;
+        age: number;
+        address: string;
+        designation: string
+
+        constructor(name: string, age: number, address: string, designation: string){
+            this.name = name;
+            this.age = age;
+            this.address = address;
+            this.designation = designation;
+        }
+
+        getSleep(sleepOfHour : number){
+            console.log(`${this.name} will sleep ${sleepOfHour}`);
+        }
+
+        takeClass(numberOfTakeClass : number){
+            console.log(`${this.name} will take ${numberOfTakeClass} classes`);
+        }
+       
+    }
+
+    const teacher = new Teacher("Mr. Kabir", 40, "Dhaka", "Professior")
+    teacher.takeClass(3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
